5dcd62dfd37584c1b9954aaf62c687e6d4415dd0
nihal2898 <158470037+nihal2898@users.noreply.github.com>
2024-02-01 15:42:20 +0000

    container config done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,9 @@
+console.log("Learning variables")
+const accountId = 900999
+let accountName = "Nihal"
+let accountEmail = 'nihalthakur81@gmail.com'
+let accountPassword = 2498
+let accountState;
+
+console.log(accountName)
+console.table([accountId, accountName, accountEmail , accountPassword, accountState ])
