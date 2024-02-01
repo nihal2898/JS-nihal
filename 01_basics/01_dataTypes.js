add65c54887326cee7f03f91c085925f1844a69b
nihal2898 <158470037+nihal2898@users.noreply.github.com>
2024-02-01 16:16:45 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,13 @@
+//when we find type of undefined = undefined//
+//when we find type of null = object //
+
+let name = "nihal"
+let rollNum = 10
+let email;
+let age = null
+
+
+console.log(typeof name)
+console.log(typeof rollNum)
+console.log(typeof email)
+console.log(typeof age);
