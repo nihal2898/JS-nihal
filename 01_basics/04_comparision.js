f2c51eed616d564b21aa2c9a9c9a884e8f151db7
Nihal Singh Thakur <158470037+nihal2898@users.noreply.github.com>
2024-02-02 17:07:40 +0000

    comparision null undefined

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,16 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2<1) //false
+// console.log(2>1) // true
+// console.log(2<=1) // false
+// console.log(2==1) // false
+// console.log(2>=1) // true
 
+// console.log("2" < 3) //true 
+// console.log(2 <= "3") // true
+// console.log("2"==1) // false
+// console.log("2" > 1) // true
 
-// console.log("2" > 1);
-// console.log("02" > 1);
-
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
-// === 
-
-console.log("2" === 2);
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
+console.log(null<0)
+// in comparision it converts null into 0(number) but while doing equity it doesnt convert null into 0(number)
