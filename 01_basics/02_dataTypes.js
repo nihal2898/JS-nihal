17b6039f6cd9a6f4a97788f4a6259b16d92acd44
Nihal Singh Thakur <158470037+nihal2898@users.noreply.github.com>
2024-02-02 12:17:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+//when we find type of undefined = undefined//
+//when we find type of null = object //
+/*
+ boolean
+ number
+ bigint
+ 
+*/
+
+let name = "nihal"
+let rollNum = 10
+let email;
+let age = null
+
+
+console.log(typeof name)
+console.log(typeof rollNum)
+console.log(typeof email)
+console.log(typeof age);
